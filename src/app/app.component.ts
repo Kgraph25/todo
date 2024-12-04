f226fe1cc93dc371d6ff1b27122044a0e5a6b8aa
Kgraph25 <muratakaito.3873@gmail.com>
2024-12-04 05:32:50 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { ItemComponent } from "./item/item.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [NgFor, NgIf, ItemComponent]
 })
 export class AppComponent {
   title = 'todo';
+
+  filter: "all" | "active" | "done" = "all"
+
+  allItems = [
+    { description: "eat", done: true},
+    { description: "sleep", done: false},
+    { description: "play", done: false},
+    { description: "laugh", done: false}
+  ];
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done :
+  !item.done
+  );
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  remove(item: { description: string; done: boolean; }) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+
 }
