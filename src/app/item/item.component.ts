86181592cd5d16af980d3cd7093755fd725d94b5
muratakaito.3873 <muratakaito.3873@gmail.com>
2024-12-04 12:31:23 +0000

    module Cookie saving task

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { Item } from '../item';
+import { AppComponent } from '../app.component';
 @Component({
   selector: 'app-item',
   imports: [NgIf],
@@ -13,12 +14,25 @@ export class ItemComponent {
 
   @Input() item!: Item;
   @Input() newItem!: string;
+  @Output() save = new EventEmitter<void>();
   @Output() remove = new EventEmitter<Item>();
 
   saveItem(description: string) {
     if (!description) return;
     this.editable = false;
     this.item.description = description;
+    this.save.emit();
+  }
+
+  appComponent: AppComponent;
+
+  constructor(appComponent:AppComponent) {
+    this.appComponent = appComponent
+  }
+
+  toggleDone() {
+    this.item.done = !this.item.done;
+    this.appComponent.saveAllItemsToCookie();
   }
 }
 
